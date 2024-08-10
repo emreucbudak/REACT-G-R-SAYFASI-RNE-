c9877df62166e26de13fde80c2dd1093ed4d97d1
Emre Üçbudak <147994857+emreucbudak@users.noreply.github.com>
2024-08-10 23:53:55 +0300

    Add files via upload

---
+++ src/Login.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Login() {
+  return (
+    <>
+    <div>
+        <form action="">
+            <div><h1>Kullanıcı Adınız = </h1></div>
+            <div><input type="text" /></div>
+            <div><h1>Şifre = </h1></div>
+            <div><input type="password" /></div>
+        </form>
+    </div>
+    </>
+  )
+}
+
+export default Login
